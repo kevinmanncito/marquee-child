c5d49ac3261e32a57527e0ca6ac86cfe3a740128
Kevin Mann <kevinrmann@gmail.com>
2018-04-09 08:16:07 -0600

    Update page-payment.php

--- page-payment.php
+++ page-payment.php
@@ -42,7 +42,7 @@ var clientToken = "<?php echo($clientToken = $gateway->clientToken()->generate()
 // Fetch the button you are using to initiate the PayPal flow
 var paypalButton = document.getElementById('paypal-button');
 
-var AMOUNT = 1.0;
+var AMOUNT = 80.0;
 
 // Create a Client component
 braintree.client.create({
